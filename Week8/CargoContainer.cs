3acecb37fe6e2fecdca0752a30ce5b1d86c1e9f8
Iryna <salagub.iruna@gmail.com>
2026-03-24 19:02:49 +0200

    Додано узагальнений клас CargoContainer<T>

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,23 @@
+﻿namespace Week8;
+
+public class CargoContainer<T> where T: DeliveryItem
+{
+    private List<T> _items = new List<T>();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+        Console . WriteLine ($"Добавлено: {item.TrackingNumber} (Тип: {typeof (T).Name})");
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+        foreach (var item in _items)
+        {
+            total += item.CalculateCost();
+        }
+
+        return total;
+    }
+}
